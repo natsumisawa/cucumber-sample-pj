a99dc208f3db8596b141d27e4eb151bef975fea9
natsumi.sawa <natsumi.sawa@bizreach.co.jp>
2018-11-23 14:52:07 +0900

    ;)

--- sample-app/app/controllers/HomeController.scala
+++ sample-app/app/controllers/HomeController.scala
@@ -1,7 +1,6 @@
 package controllers
 
 import javax.inject._
-import play.api._
 import play.api.mvc._
 
 /**
@@ -9,16 +8,13 @@ import play.api.mvc._
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(messagesAction: MessagesActionBuilder, cc: ControllerComponents) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
+  def index() = Action { implicit req =>
     Ok(views.html.index())
+  }
+
+  def vote() = Action { implicit req =>
+    Ok(views.html.vote())
   }
 }
---
+++ sample-app/app/modules/NogiModule.scala
@@ -0,0 +1,8 @@
+package modules
+import com.google.inject.AbstractModule
+import play.api.i18n.MessagesProvider
+
+//class NogiModule extends AbstractModule {
+//  override def configure(): Unit =
+//    bind(classOf[MessagesProvider]).to(classOf[MessagesProvider])
+//}
